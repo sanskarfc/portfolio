123b2e17c1792e926f02eba0faf4ca32710d11e9
Sanskar Sharma <sanskarfc@gmail.com>
2023-12-02 14:26:28 +0530

    restart

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 
-const Cube = () => {
+const Checker = () => {
   return (
-    <mesh>
-      <boxGeometry args={[2, 2, 2]} /> 
-    </mesh>
-  );
-};
-
-const MyThreeJsComponent = () => {
-  const canvasStyle = {
-    background: "black",
-  };
-
-  return (
-    <div style={canvasStyle}>
-      <Canvas
-        camera={{ position: [0, 0, 5], fov: 75 }} // Adjusted camera settings
-      >
-        <Cube />
-        <OrbitControls />
-      </Canvas>
+    <div> 
+      Hey!
     </div>
   );
 };
 
-export default MyThreeJsComponent;
+export default Checker;
